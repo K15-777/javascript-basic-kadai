3455fa884285e740049b45eb44cbe74ac5ada839
K15-777 <kizmk15@gmail.com>
2026-07-07 00:09:29 +0900

    Add files via upload

---
+++ kadai_020/event.js
@@ -0,0 +1,14 @@
+const btn = document.querySelector("#btn");
+
+
+
+btn.addEventListener("click", () => {
+
+    const text = document.querySelector("#text");
+
+    text.textContent = "ボタンをクリックしました"
+
+}
+
+); 
+
